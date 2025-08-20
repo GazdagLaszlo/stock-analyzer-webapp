b08783faee312252d2facd550218c5944b661c3c
GazdagLaszlo <glaci0206@gmail.com>
2025-08-20 13:19:50 +0200

    Implement StockUpdaterService - GetSP500Stocks with background service

---
+++ backend/StockManager/StockManager.Services/StockUpdaterBackgroundService.cs
@@ -0,0 +1,42 @@
+﻿    
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StockManager.DataContext.Entities;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using StockManager.DataContext.DTOs;
+
+namespace StockManager.Services
+{
+    public class StockUpdaterBackgroundService : BackgroundService
+    {
+        public readonly IServiceScopeFactory _scopeFactory;
+
+        public StockUpdaterBackgroundService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    using var scope = _scopeFactory.CreateScope();
+                    var updaterService = scope.ServiceProvider.GetRequiredService<StockUpdaterService>();
+                    await updaterService.GetSP500Stocks();
+
+                    await Task.Delay(TimeSpan.FromDays(1), stoppingToken);                                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Hiba a háttérszolgáltatásban: {ex.Message}");
+                    await Task.Delay(5000, stoppingToken);
+                }
+            }
+        }
+    }
+}
---
+++ backend/StockManager/StockManager.Services/StockUpdaterService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StockManager.DataContext.DTOs;
+using StockManager.Services;
+
+namespace StockManager.Services
+{
+    public class Metric
+    {
+        public double marketCapitalization { get; set; }   
+    }
+    public class MetricResponse
+    {
+        public Metric metric { get; set; }
+    }
+    public class StockUpdaterService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IStockService _stockService;
+
+        public StockUpdaterService(HttpClient httpClient, IStockService stockService)
+        {
+            _httpClient = httpClient;
+            _stockService = stockService;
+        }
+
+        public async Task GetSP500Stocks()
+        {
+            string ninjaApiKey = "3ILBgQDAH8BATGJp0aPiYA==0y3W4FYJYdhruc2g";
+            _httpClient.DefaultRequestHeaders.Add("X-Api-Key", ninjaApiKey);
+            var ninjaResponse = await _httpClient.GetStringAsync("https://api.api-ninjas.com/v1/sp500");
+            
+
+
+            string finnhubApiKey = "d2e57s1r01qjrul642d0d2e57s1r01qjrul642dg";
+
+            /*
+            var url = $"https://finnhub.io/api/v1/stock/symbol?exchange=US&token={apiKey}";
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            var companys = JsonSerializer.Deserialize<List<StockCreateDto>>(responseString);
+            */
+            var companys = JsonSerializer.Deserialize<List<StockCreateDto>>(ninjaResponse);
+
+            var allStocks = await _stockService.GetAllAsync();
+
+            foreach (var company in companys)
+            {
+                var getStock = $"https://finnhub.io/api/v1/stock/metric?symbol={company.Symbol}&metric=all&token={finnhubApiKey}";
+                var response = await _httpClient.GetAsync(getStock);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Nem sikerült lekérni {company.Symbol}, status: {response.StatusCode}");
+                    continue;
+                }
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                var stock = JsonSerializer.Deserialize<MetricResponse>(responseString);
+
+                if (!allStocks.Any(x => x.Symbol == company.Symbol))
+                {
+
+                    var dto = new StockCreateDto
+                    {
+                        Symbol = company.Symbol,
+                        MarketCap = stock.metric.marketCapitalization,
+                        CompanyName = company.CompanyName,
+                        Sector = company.Sector,
+                    };
+                    await _stockService.CreateAsync(dto);
+                }
+                else
+                {
+                    var stockToUpdate = allStocks.FirstOrDefault(x => x.Symbol.Equals(company.Symbol));
+
+                    var updateDto = new StockUpdateDto
+                    {
+                        Symbol = company.Symbol,
+                        MarketCapitalization = stock.metric.marketCapitalization,
+                        CompanyName = company.CompanyName,
+                        Sector = company.Sector,
+                    };
+
+                    await _stockService.UpdateAsync(stockToUpdate.Id, updateDto);
+                }
+
+                //60 request/min is the max capacity on Finnhub.io
+                await Task.Delay(1200);
+            }
+        }
+    }
+}
