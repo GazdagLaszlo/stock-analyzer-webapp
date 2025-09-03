948230e2c6bac0a3355689787a7f29c02c58f87a
GazdagLaszlo <glaci0206@gmail.com>
2025-09-03 09:28:24 +0200

    Setup react project, generate frontend from swagger.json

---
+++ frontend/StockManager/generated-sources/openapi/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './PortfolioApi';
+export * from './PortfolioItemApi';
+export * from './StockApi';
+export * from './StockDataApi';
+export * from './TransactionApi';
+export * from './UserApi';
+export * from './WatchListApi';
+export * from './WatchListItemApi';
---
+++ frontend/StockManager/generated-sources/openapi/models/index.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './LoginResponse';
+export * from './PortfolioCreateDto';
+export * from './PortfolioDto';
+export * from './PortfolioItemCreateDto';
+export * from './PortfolioItemDto';
+export * from './PortfolioItemUpdateDto';
+export * from './PortfolioUpdateDto';
+export * from './StockCreateDto';
+export * from './StockDataCreateDto';
+export * from './StockDataDto';
+export * from './StockDataUpdateDto';
+export * from './StockDto';
+export * from './StockUpdateDto';
+export * from './TransactionCreateDto';
+export * from './TransactionDto';
+export * from './TransactionType';
+export * from './TransactionUpdateDto';
+export * from './UserCreateDto';
+export * from './UserDto';
+export * from './UserLoginDto';
+export * from './UserUpdateDto';
+export * from './WatchListDto';
+export * from './WatchListItemCreateDto';
+export * from './WatchListItemDto';
+export * from './WatchListItemUpdateDto';
